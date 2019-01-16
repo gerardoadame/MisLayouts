a76b72526b99ef959a5021519732bb6fd7f4bcd8
gerardo <gera_skate@outlook.com>
2019-01-16 00:51:45 -0600

    ES LEFT

---
+++ app/src/main/java/com/example/gerardo/mislayouts/MainActivity.java
@@ -0,0 +1,81 @@
+package com.example.gerardo.mislayouts;
+
+import android.os.CountDownTimer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+    Button btn1, boton;
+    TextView texto;
+    int contador=0;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        boton=findViewById(R.id.btncentro);
+        texto=findViewById(R.id.texto);
+        texto.setText(R.string.sinnombre);
+        boton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                contador++;
+                texto.setText(String.valueOf(contador));
+            }
+        });
+
+
+
+
+//        btn1=findViewById(R.id.miboton);
+
+
+        btn1=findViewById(R.id.btn1);
+
+        new CountDownTimer(100000,100)
+        {
+          @Override
+          public void onTick(long millisUntilFinished)
+          {
+              Random random =new Random();
+              int rand=random.nextInt(2);
+              FrameLayout.LayoutParams lp=new FrameLayout.LayoutParams(-2,-2);
+//              lp.gravity=Gravity.TOP;
+//              btn1.setLayoutParams(lp);
+
+
+
+
+              switch (rand)
+              {
+                  case 0: lp.gravity=Gravity.CENTER;break;
+                  case 1: lp.gravity=Gravity.LEFT;break;
+              }
+
+
+          }
+          @Override
+            public void onFinish()
+          {
+
+          }
+
+
+
+        }.start();
+
+
+
+    }
+}
