d95f7d2526ede938da4d6af1b92cc5ec826dc37a
gerardo <gera_skate@outlook.com>
2019-02-02 17:19:29 -0600

    Mejora del 1 proyecto. El boton se muve en posiciones diferentes y al dalre click al boton se agregara a un textview

--- app/src/main/java/com/example/gerardo/mislayouts/MainActivity.java
+++ app/src/main/java/com/example/gerardo/mislayouts/MainActivity.java
@@ -3,11 +3,13 @@ package com.example.gerardo.mislayouts;
 import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Random;
 
@@ -22,11 +24,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn1=findViewById(R.id.btn1);
+        btn1=findViewById(R.id.btncentro);
+
         boton=findViewById(R.id.btncentro);
         texto=findViewById(R.id.texto);
         texto.setText(R.string.sinnombre);
-        boton.setOnClickListener(new View.OnClickListener()
+        btn1.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View v)
@@ -36,34 +39,44 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
-
+        View.OnClickListener clickboton=new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                contador++;
+                texto.setText(String.valueOf(contador));
+            }
+        };
 
 //        btn1=findViewById(R.id.miboton);
-
-
-
 
         new CountDownTimer(100000,100)
         {
           @Override
           public void onTick(long millisUntilFinished)
           {
+
+
               Random random =new Random();
-              int rand=random.nextInt(2);
+              int rand=random.nextInt(4)+1;
+//              se pone +1 porque inica desde el cero y con el +1 inicia desde el 1
               FrameLayout.LayoutParams lp=new FrameLayout.LayoutParams(-2,-2);
 //              lp.gravity=Gravity.TOP;
 //              btn1.setLayoutParams(lp);
 
 
 
-
+              Log.i("num",String.valueOf(rand));
               switch (rand)
               {
-                  case 0: lp.gravity=Gravity.CENTER;break;
-                  case 1: lp.gravity=Gravity.RIGHT;break;
+                  case 1: lp.gravity=Gravity.LEFT;break;
+                  case 2: lp.gravity=Gravity.RIGHT;break;
+                  case 3: lp.gravity=Gravity.CENTER_HORIZONTAL;break;
+                  case 4: lp.gravity=Gravity.BOTTOM;break;
+
+
               }
               btn1.setLayoutParams(lp);
+
 
 
           }
